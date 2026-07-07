e21856296762d770ab080c27b6b005edebf8cf5e
Abdams <Abdulrazaqmalik21@gmail.com>
2026-07-07 09:49:48 +0100

    changes

--- config.py
+++ config.py
@@ -13,3 +13,8 @@ class Keys:
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SQLALCHEMY_ENGINE_OPTIONS = {
+            "pool_pre_ping": True,
+            "pool_recycle": 300,
+        }
